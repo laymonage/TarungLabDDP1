2380e5074426f1c0006eaef1c08c9f213c834458
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2018-06-12 16:09:56 +0700

    Add lab 11 template for class C

---
+++ lab/11/template_11_c.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+class Kalkulator():
+    def __init__(self, master):
+        self.master = master
+        # TODO: Set title window menjadi "Kalkulator Sederhana" di bawah ini.
+
+        # TODO: Buatlah Label, Entry, Button,
+        # dan komponen-komponen lain yang dibutuhkan.
+
+    def tambah(self):
+        # TODO: Implementasikan fungsi tambah, hapus "pass"
+        pass
+
+    def kurang(self):
+        # TODO: Implementasikan fungsi kurang, hapus "pass"
+        pass
+
+    def kali(self):
+        # TODO: Implementasikan fungsi kali, hapus "pass"
+        pass
+
+    def bagi(self):
+        # TODO: Implementasikan fungsi bagi, hapus "pass"
+        pass
+
+root = Tk()
+gui = Kalkulator(root)
+root.mainloop()
+
+
